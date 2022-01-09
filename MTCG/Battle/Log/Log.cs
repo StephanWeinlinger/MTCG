74ff7d207d8befe336fc63746b0e00c8fddc3071
StephanWeinlinger <stephan.weinlinger@gmail.com>
2022-01-10 00:40:58 +0100

    Add battle logic

---
+++ MTCG/Battle/Log/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG.Battle.Card;
+
+namespace MTCG.Battle.Log {
+	class Log {
+		public int WinnerId;
+		public int LoserId;
+		public bool IsDraw;
+		public IList<LogEntry> Rounds;
+
+		public Log() {
+			WinnerId = -1;
+			LoserId = -1;
+			IsDraw = false;
+			Rounds = new List<LogEntry>();
+		}
+
+		public void AddEntry(ICard winner, ICard loser, int winnerDamage, int loserDamage, bool isDraw) {
+			Rounds.Add(new LogEntry(winner, loser, winnerDamage, loserDamage, isDraw));
+		}
+	}
+}
---
+++ MTCG/Battle/Log/LogEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTCG.Battle.Card;
+
+namespace MTCG.Battle.Log {
+	class LogEntry {
+		private int _actWinnerDamage;
+		private int _actLoserDamage;
+		private int _baseWinnerDamage;
+		private int _baseLoserDamage;
+		private string _winnerCard;
+		private string _loserCard;
+		private string _message;
+
+		public LogEntry(ICard winner, ICard loser, int winnerDamage, int loserDamage, bool isDraw) {
+			_actWinnerDamage = winnerDamage;
+			_actLoserDamage = loserDamage;
+			_baseWinnerDamage = winner.Damage;
+			_baseLoserDamage = loser.Damage;
+			_winnerCard = $"{winner.Name.ToString()} {winner.Type.ToString()} {winner.Element.ToString()}"; // Name, Type, Element
+			_loserCard = $"{loser.Name.ToString()} {loser.Type.ToString()} {loser.Element.ToString()}"; // Name, Type, Element
+			_message = isDraw ? $"{winner.Name.ToString()} tied against {loser.Name.ToString()}" : $"{winner.Name.ToString()} won against {loser.Name.ToString()}";
+			Console.WriteLine(_message);
+		}
+	}
+}
